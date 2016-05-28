8c88e45f738c77b1d9244d04d54e65b65dd75afa
BDSword <thetruebloodsword@gmail.com>
2016-05-28 19:09:30 +0800

    Add migration to change user name to not null with default value

---
+++ db/migrate/20160528073110_change_user_name_to_not_null.rb
@@ -0,0 +1,9 @@
+class ChangeUserNameToNotNull < ActiveRecord::Migration
+  def up
+    change_column :users, :name, :string, null: false, default: ''
+  end
+
+  def down
+    change_column :users, :name, :string, null: true, default: nil
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528054455) do
+ActiveRecord::Schema.define(version: 20160528073110) do
 
   create_table "taps", force: :cascade do |t|
     t.string   "location",   limit: 255
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160528054455) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   limit: 255
+    t.string   "name",                   limit: 255, default: "", null: false
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
