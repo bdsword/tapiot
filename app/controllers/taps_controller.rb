e65de82de87aea3e74a8d901ccab387339b8b290
tjchen2015 <tzujung2627@gmail.com>
2016-05-28 22:31:53 +0800

    Add taps basic views

--- app/controllers/taps_controller.rb
+++ app/controllers/taps_controller.rb
@@ -7,24 +7,41 @@ class TapsController < ApplicationController
   end
 
   def show
+    @tap = Tap.find(params[:id])
   end
 
   def new
+    @tap = Tap.new
   end
 
   def edit
-
+    @tap = Tap.find(params[:id])
   end
 
   def create
+    @tap = Tap.new(tap_params)
+    @tap.save
 
+    redirect_to :action => :index
   end
 
   def update
+    @tap = Tap.find(params[:id])
+    @tap.update(tap_params)
 
+    redirect_to :action => :index
   end
 
   def destroy
+    @tap = Tap.find(params[:id])
+    @tap.destroy
 
+    redirect_to :action => :index
+  end
+
+  private
+
+  def tap_params
+    params.require(:tap).permit(:location, :created_at)
   end
 end
