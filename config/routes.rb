bc3257c771b2c151503d96fbcb9b98b464ee3fc4
BDSword <thetruebloodsword@gmail.com>
2016-06-05 23:27:09 +0800

    Add route related to web active on/off

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
     member do
       post 'on', to: 'taps#turn_on'
       post 'off', to: 'taps#turn_off'
+      post 'web_on', to: 'taps#web_turn_on'
+      post 'web_off', to: 'taps#web_turn_off'
     end
+    post 'web_off_update', on: :collection, to: 'taps#web_turn_off_update'
   end
 
   resources :users, only: [:index, :show]
