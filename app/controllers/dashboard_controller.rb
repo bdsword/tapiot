b5d7237b5da53177f713afa73aadc47c8e15c4b3
tjchen2015 <tzujung2627@gmail.com>
2016-06-07 11:39:25 +0800

    Add charts to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,9 +7,11 @@ class DashboardController < ApplicationController
     @search = WaterUse.search(params[:q])
     @water_uses_1 = @search.result.where(user_id: current_user.id, created_at: (Time.now - 1.month)..Time.now)
 
-    @water_uses_2
-    @water_uses_3
+    @water_uses_2 = @search.result.group(:tap_id).order('COUNT(id) DESC').count(:id)
+    @water_uses_3 = @search.result.group(:tap_id).order('SUM(water_consumed) DESC').sum(:water_consumed)
 
     @water_uses_4 = @search.result.order(water_consumed: :desc).limit(10)
+
+    @water_uses_5 = @search.result.where(created_at: (Time.now - 1.month)..Time.now)
   end
 end
